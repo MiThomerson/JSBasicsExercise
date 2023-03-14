afb90d15b2ea4748222d6ba691c0757bec474211
Mic Thomerson <mic732@gmail.com>
2023-03-14 11:47:47 -0500

    Initial commit for excercise.

---
+++ script.js
@@ -0,0 +1,47 @@
+// 1. 
+console.log(23);
+
+// 2. 
+
+// boolean string number null undefined
+
+// 3.
+
+// +,-,**,*,/,%
+
+// 4. 
+console.log(5%2);
+
+// 5. 
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6. 
+console.log("Hawk " + "eye");
+
+// 7. 
+console.log("Michael".length + "Thomerson".length);
+
+// 8. 
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+
+// 1. 
+console.log("\\");
+
+// 2. 
+console.log(5**4-9);
+
+// 3. 
+console.log((("I".length + "Am".length) % "Iron".length) % "Man".length);
+
+// 4. 
+names = "CAP VISION OKOYE NICK FURY GROOT DOCTOR STRANGE THOR STAR-LORD";
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+// 5. 
+m = 25
+c = 4
+console.log(m+c);
